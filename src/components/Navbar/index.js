c848c914d4880ae870ce3a2eb58eaf9c5d53f564
Lionel Savidan <liosavid@gmail.com>
2021-02-04 18:38:46 +0000

    Design finished

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Bars, Nav, NavIcon, NavLink} from './NavbarElements';
+
+export const Navbar = ({toggle}) => {
+    return (
+        <>
+            <Nav>
+                <NavLink to='/'>Pizza</NavLink>
+                <NavIcon onClick={toggle}>
+                    <p>Menu</p>
+                    <Bars />
+                </NavIcon>
+            </Nav>
+    
+        </>
+    );
+};
